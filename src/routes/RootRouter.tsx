b17c5c053fe9e6911632e19c8881a17513d862f2
Fernanda Mendonça <fernandasmendonca@outlook.com>
2022-07-06 01:43:42 -0300

    feat - add mui lib and react-router-dom, add beginners routes, create the files structure and add the initial components

--- src/routes/RootRouter.tsx
+++ src/routes/RootRouter.tsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import {
-  Route,
-  Routes
+  BrowserRouter as Router, Route
 } from 'react-router-dom'
 
-import Dashboard from './content/Dashboard/Dashboard'
-import Listing from './content/Listing/Listing'
-import Login from './content/Login/Login'
-import SignUp from './content/SignUp/SignUp'
+import Dashboard from '../content/Dashboard/Dashboard'
+import Listing from '../content/Listing/Listing'
+import Login from '../content/Login/Login'
+import SignUp from '../content/SignUp/SignUp'
 
-function App() {
+export default function RootRouter() {
+
   return (
-    <Routes>
+    <Router>
       <Route element={<Login />} path="/login" />
       <Route element={<Dashboard />} path="/dashboard" />
       <Route element={<Listing />} path="/listing" />
       <Route element={<SignUp />} path="/sign-up" />
-    </Routes>
+    </Router>
   )
 }
-
-export default App
